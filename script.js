026598aa4ca6a269310a28287cb65a9421a7b978
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-02-27 22:15:06 -0300

    update

---
+++ script.js
@@ -0,0 +1,17 @@
+// MENU HAMBURGUER NAVBAR
+function initMenuMobile(){
+  const btnMobile = document.querySelector(".btn-mobile");
+  const headerMenu = document.querySelector(".header-menu");
+
+  function ToggleMenu() {
+    const nav = document.querySelector('.nav');
+    const headerMenu = document.querySelector('.header-menu');
+    nav.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+  }
+  
+  btnMobile.addEventListener("click", ToggleMenu);
+  headerMenu.addEventListener("click", ToggleMenu);
+}
+initMenuMobile();
+// MENU HAMBURGUER NAVBAR//
