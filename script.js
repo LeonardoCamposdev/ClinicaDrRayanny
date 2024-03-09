83a41989be5a4da09ea8754c5d255cc300024638
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-03-09 00:31:41 -0300

    update

--- script.js
+++ script.js
@@ -30,3 +30,21 @@ imagens.forEach((imagem) => {
   imagem.addEventListener("click", galeriaTrocar);
 });
 // CONHEÇA O ESPAÇO TROCA DE IMG PRINCIPAL!!!
+
+// PERGUNTAS FREQUENTES
+const accordionList = document.querySelectorAll(".dt-pergunta");
+const activeClass = "ativo";
+
+accordionList[0].classList.add(activeClass);
+accordionList[0].nextElementSibling.classList.add(activeClass);
+
+function activeAccordion() {
+  this.classList.toggle(activeClass);
+  this.nextElementSibling.classList.toggle(activeClass);
+}
+
+accordionList.forEach((item) => {
+  item.addEventListener("click", activeAccordion);
+});
+
+activeAccordion();
